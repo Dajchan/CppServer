c07997d2e3951625333816da348121458f15b16d
Jonathan Cichon <jcic@pixelflut.net>
2016-03-01 14:03:41 +0100

    configurate the server on startup

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,11 @@ int main (int argc, char *const *argv) {
     const char *directory = "/Users/skar/Desktop/lextest";
     short port = 3001;
     if (argc == 3) {
-//        port =
-//        directory =
+        port = stoi(argv[1]);
+        directory = argv[2];
+    } else {
+        printf("usage:\npx-cpp-server PORT HOME_DIR \n");
+        return -1;
     }
     return startup_server(port, directory);
 }
